7f2c220da2bfe87b159f46d46cabb18c3fc3b6bb
BhargavR47 <79641490+BhargavR47@users.noreply.github.com>
2022-11-18 20:18:30 -0700

    Add files via upload

---
+++ drawImages/Voltage_reading.ino
@@ -0,0 +1,26 @@
+
+
+float floatMap(float x, float in_min, float in_max, float out_min, float out_max) {
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+// the setup routine runs once when you press reset:
+void setup() {
+  // initialize serial communication at 9600 bits per second:
+  Serial.begin(9600);
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+  // read the input on analog pin GIOP36:
+  int analogValue = analogRead(12);
+  // Rescale to potentiometer's voltage (from 0V to 3.3V):
+  float voltage = floatMap(analogValue, 0, 4095, 0, 3.3);
+
+  // print out the value you read:
+  Serial.print("Analog: ");
+  Serial.print(analogValue);
+  Serial.print(", Voltage: ");
+  Serial.println(voltage);
+  delay(1000);
+}
